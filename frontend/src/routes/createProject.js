241bd03b795c5c1e3b523b9d8246ad0ab91639a7
ZindaRoud <23100250@lums.edu.pk>
2022-11-18 20:06:25 +0500

    someChanges

--- frontend/src/routes/createProject.js
+++ frontend/src/routes/createProject.js
@@ -13,7 +13,7 @@ function CreateProject()
     return (
         <div className = "App">
             <h1>Create Project</h1>
-            <form onSubmit={(e) => createProject(e)}>
+            <form onSubmit={(e) => createProject2(e)}>
                 <label> Project Name: </label><br/>
                 <input type="text" id='form_name'></input><br/>
                 <label> Project Description: </label><br/>
@@ -21,35 +21,32 @@ function CreateProject()
                 <label> Assigned to: </label><br/>
                 <input type="text" id='assigned_to'></input><br/>
                 <label> Start Time : </label><br/>
-                <input type = "Date" id = "date" ></input><br/>
+                <input type = "Date" id = "date_start" ></input><br/>
                 <label > End Time : </label><br/>
-                <input type = "Date" id = "date" ></input><br/>
-                {/* <label> Status: </label><br/> */}
-                {/* <select id  = "status">
-                    <option value = "in progress">in progress</option>
-                    <option value = "completed">completed</option>
-                    <option value = "canceled">canceled</option>
-                </select>
-                <br/> */}
+                <input type = "Date" id = "date_end" ></input><br/>
                 <label> Revenue : </label> <br/>
                 <input type = "number" id = "revenue"></input><br/>
                 <label> Client ID : </label><br/>
                 <input type = "number" id = "client_id"></input><br/>
-                {/* <label >Assigned To </label> <br/>
-                <input type = "text" id = "assigned_to"></input><br/> */}
-
                 <br/><input type="submit" id='submit_create_form' ></input>
             </form>
         </div>
     );
 }
 
-function createProject(e)
+function createProject2(e)
 {
     e.preventDefault();
     let request = {
         form_name: document.getElementById('form_name').value,
-        form_description: document.getElementById('form_description').value }
+        form_description: document.getElementById('form_description').value ,
+        assigned_to: document.getElementById('assigned_to').value,
+        start_time: document.getElementById('date_start').value,
+        end_time: document.getElementById('date_end').value,
+        // status: document.getElementById('status').value,
+        revenue: document.getElementById('revenue').value,
+        client_id: document.getElementById('client_id').value
+     }
     axios.post('http://localhost:3000/createProject', request)
         .then(resp => {
             alert(resp.data.message);

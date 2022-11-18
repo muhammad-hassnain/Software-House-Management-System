1e710b1edf7bacd1baed72f07b9fc4d0b7089e6a
ZindaRoud <23100250@lums.edu.pk>
2022-11-18 19:16:08 +0500

    form for creating a project

--- frontend/src/routes/createProject.js
+++ frontend/src/routes/createProject.js
@@ -1,18 +1,44 @@
-import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import axios from 'axios';
+import { Dropdown } from 'bootstrap';
+
+// const options = [
+//     {value : "in progress", label : "in progress"},
+//     {value : "completed", label : "completed"},
+//     {value : "canceled", label : "canceled"}
+// ]
 
 function CreateProject()
 {
     return (
         <div className = "App">
-            <h1>Create Form</h1>
+            <h1>Create Project</h1>
             <form onSubmit={(e) => createProject(e)}>
-                <label> Form Name: </label><br/>
+                <label> Project Name: </label><br/>
                 <input type="text" id='form_name'></input><br/>
-                <label> Form Description: </label><br/>
-                <input type="text" id='form_description'></input><br/><br/>
-                <input type="submit" id='submit_create_form' ></input>
+                <label> Project Description: </label><br/>
+                <input type="text" id='form_description'></input><br/>
+                <label> Assigned to: </label><br/>
+                <input type="text" id='assigned_to'></input><br/>
+                <label> Start Time : </label><br/>
+                <input type = "Date" id = "date" ></input><br/>
+                <label > End Time : </label><br/>
+                <input type = "Date" id = "date" ></input><br/>
+                {/* <label> Status: </label><br/> */}
+                {/* <select id  = "status">
+                    <option value = "in progress">in progress</option>
+                    <option value = "completed">completed</option>
+                    <option value = "canceled">canceled</option>
+                </select>
+                <br/> */}
+                <label> Revenue : </label> <br/>
+                <input type = "number" id = "revenue"></input><br/>
+                <label> Client ID : </label><br/>
+                <input type = "number" id = "client_id"></input><br/>
+                {/* <label >Assigned To </label> <br/>
+                <input type = "text" id = "assigned_to"></input><br/> */}
+
+                <br/><input type="submit" id='submit_create_form' ></input>
             </form>
         </div>
     );

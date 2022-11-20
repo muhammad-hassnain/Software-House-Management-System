4d72525d4812ecbc83cb813297ca83375f4efd62
umar <23100206@lums.edu.pk>
2022-11-20 19:06:48 +0500

    setting up backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Abdullah!");
 });

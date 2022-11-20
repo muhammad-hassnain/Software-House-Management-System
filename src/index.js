be69bb399d5e8d8b3b6cd1a96ef2ead14f1f7d99
umar <23100206@lums.edu.pk>
2022-11-20 15:50:52 +0500

    added routing, created error page, signup page (frontend) along with validation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
+// import ErrorPage from "./pages/ErrorPage";
+import Routes from "./pages/Routes";
+// import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//   },
+// ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <RouterProvider router={router} /> */}
+    <Routes></Routes>
   </React.StrictMode>
 );
